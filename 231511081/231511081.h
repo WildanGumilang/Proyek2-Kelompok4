ebe4a1f9b55c13ea69ceff275e19a078339110fc
Muhamad Haikal Hariyanto <haikaluhuy123@gmail.com>
2024-03-11 09:28:07 +0700

    ganti.h dan cpp

--- 231511081/231511081.h
+++ 231511081/231511081.h
@@ -1,57 +1,11 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
+#include "231511081.cpp"
 using namespace std;
 
-// Definisi struct UserInfo
-struct UserInfo {
-    string nik;
-    string namalengkap;
-    string tanggallahir;
-    string tanggalperiksa;
-    int pilihandokter;
-    int carabayar;
-};
-
 // Fungsi untuk mengambil informasi pasien dari file daftarperiksa.txt
-void ambilInformasiPasien(const string& nik, UserInfo& user) {
-    ifstream inFile("daftarperiksa.txt");
-    if (inFile.is_open()) {
-        string line;
-        while (getline(inFile, line)) {
-            string currentNik = line.substr(0, line.find('|'));
-            if (currentNik == nik) {
-                // Jika NIK pasien cocok, mengisi informasi pasien dari file
-                user.nik = currentNik;
-                size_t pos = line.find('|', currentNik.length() + 1);
-                user.namalengkap = line.substr(currentNik.length() + 1, pos - currentNik.length() - 1);
-                pos = line.find('|', pos + 1);
-                user.tanggallahir = line.substr(user.namalengkap.length() + currentNik.length() + 2, pos - user.namalengkap.length() - currentNik.length() - 2);
-                pos = line.find('|', pos + 1);
-                user.tanggalperiksa = line.substr(pos + 1);
-                break;
-            }
-        }
-        inFile.close();
-    } else {
-        cout << "Gagal membuka file daftarperiksa.txt." << endl;
-    }
-}
+void ambilInformasiPasien(const string& nik, UserInfo& user);
 
 // Fungsi untuk mengirim hasil pemeriksaan dari dokter ke pasien
-void kirimHasilPemeriksaan(const UserInfo& user, const string& hasilPemeriksaan, const string& resepObat) {
-    // Membuka file hasilperiksa.txt untuk menambahkan hasil pemeriksaan
-    ofstream outFile("hasilperiksa.txt", ios::app);
-    if (outFile.is_open()) {
-        // Menambahkan hasil pemeriksaan ke file
-        outFile << user.nik << "|" << user.tanggalperiksa << "|" << hasilPemeriksaan << "|" << resepObat << endl;
-        outFile.close();
-        cout << "Hasil pemeriksaan berhasil dikirim ke pasien." << endl;
-    } else {
-        cout << "Gagal membuka file hasilperiksa.txt." << endl;
-    }
-}
+void kirimHasilPemeriksaan(const UserInfo& user, const string& hasilPemeriksaan, const string& resepObat);
 
 int main() {
     // Contoh penggunaan untuk dokter

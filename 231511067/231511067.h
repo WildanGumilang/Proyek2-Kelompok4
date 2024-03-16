e2ad358dd47974633eccacc85ef6505709fa7751
thrdplnt <agraanisa0013@gmail.com>
2024-03-16 15:18:29 +0700

    mengubah file

--- 231511067/231511067.h
+++ 231511067/231511067.h
@@ -1,2 +1,13 @@
 #include "231511067.cpp"
+#ifndef HILL_CIPHER_H
+#define HILL_CIPHER_H
 
+#include <string>
+
+using namespace std;
+
+void hill_cipher_encrypt(const char* text, const int key[2][2], char* encrypted_text);
+void saveToFile(const char* filename, const char* original, const char* encrypted);
+void tampilkanDataPasienByNIK(const std::string& nik);
+
+#endif 

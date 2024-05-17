d24ea6d85b8fb396388f28eb6b308b425c8d65d5
Melly Dwiliani <mellydwiliani7@gmail.com>
2024-05-18 00:09:12 +0700

    menambah fungsi membuat node, membuat linked list

--- 231511080/231511080.h
+++ 231511080/231511080.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <cstdlib>
 #include <ctime>
+#include <string>
 
 using namespace std;
 
@@ -19,6 +20,10 @@ struct userDaftar {
     string carabayar;
 };
 
+typedef struct NodeK *kAddr;
+
 string generateNomorPendaftaran(); //fungsi untuk menghasilkan nomor pendaftaran acak
 void pendaftaranPeriksa(const string& nik, const string& namalengkap, const string& tanggallahir); //fungsi untuk mengisi pendaftaran pemeriksaan
 void tampilkanDataPeriksa(); //menampilkan data pendaftaran periksa
+kAddr createNodeKey(int nilai);
+kAddr buatLinkedListKey(int Key11, int Key12, int Key21, int Key22); 

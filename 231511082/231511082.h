45fc6083dea4006962a00c7dd6634e184e3dd6fb
Muhammad Daffa Tridya Atha <cringe.abis@gmail.com>
2024-05-19 18:26:47 +0700

    penambhan .h

--- 231511082/231511082.h
+++ 231511082/231511082.h
@@ -9,3 +9,6 @@ using namespace std;
 
 string hill_cipher_decrypt(const string& encrypted_text);
 void tampilkanSeluruhDataPasien();
+string addAngka(string str, bool genap);
+string chekNumChar(string str, bool& genap);
+tAddr bacaTabelKonversi();

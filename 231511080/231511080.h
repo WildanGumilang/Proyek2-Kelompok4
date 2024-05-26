6a8c8f6e4a1b467f74c6b1e2f8add6153229dfb8
Melly Dwiliani <mellydwiliani7@gmail.com>
2024-05-26 18:19:51 +0700

    memperbaiki

--- 231511080/231511080.h
+++ 231511080/231511080.h
@@ -11,7 +11,7 @@
 using namespace std;
 
 string generateNomorPendaftaran(); //fungsi untuk menghasilkan nomor pendaftaran acak
-bool pendaftaranPeriksa(const string& nik, const string& namalengkap, const string& tanggallahir); //fungsi untuk mengisi pendaftaran pemeriksaan
+bool pendaftaranPeriksa(const string& nik, const string& namalengkap, const string& tanggallahir, tAddr awalT, kAddr awalK, kAddr awalKinv); //fungsi untuk mengisi pendaftaran pemeriksaan
 void tampilkanDataPeriksa(tAddr awalT, kAddr awalKinv); //menampilkan data pendaftaran periksa
 kAddr createNodeKey(int nilai);
 kAddr buatLinkedListKey(int Key11, int Key12, int Key21, int Key22); 

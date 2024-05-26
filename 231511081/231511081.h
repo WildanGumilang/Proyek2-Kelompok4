ca02445e4fb62c809be5dac8bb6ffd43fb0b3d09
Muhamad Haikal Hariyanto <haikaluhuy123@gmail.com>
2024-05-26 12:07:50 +0700

    last day

--- 231511081/231511081.h
+++ 231511081/231511081.h
@@ -10,8 +10,8 @@
 
 
 bool insertAkhir(int nilai, pAddr& awal, pAddr& akhir);
-void tampilkanSuratHasilPemeriksaan(string& targetNik);
-bool buatSuratHasilPemeriksaan();
+void tampilkanSuratHasilPemeriksaan(string& targetNik, tAddr awalT, kAddr awalK, kAddr awalKinv);
+bool buatSuratHasilPemeriksaan(tAddr awalT, kAddr awalK);
 pAddr konversiPlainteksKeAngka(const string& plainteks, tAddr awal);
 int cariIndeks(tAddr head, char karakter);
 bool insertTengahUserData(const UserData& data, dtAddr& awal, dtAddr& akhir);
